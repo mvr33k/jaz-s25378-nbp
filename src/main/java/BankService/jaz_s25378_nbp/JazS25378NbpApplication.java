e3c3fec68a925c02964e5bd997764a58c75feb36
mvr3k <61803073+mvr33k@users.noreply.github.com>
2024-06-29 10:11:17 +0200

    Initial commit

---
+++ src/main/java/BankService/jaz_s25378_nbp/JazS25378NbpApplication.java
@@ -0,0 +1,13 @@
+package BankService.jaz_s25378_nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS25378NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS25378NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BankService/jaz_s25378_nbp/JazS25378NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package BankService.jaz_s25378_nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS25378NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
