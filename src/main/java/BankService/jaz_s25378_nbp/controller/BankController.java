e40c8d5c5bf0204e3a7c961e5530e529aec57329
mvr3k <61803073+mvr33k@users.noreply.github.com>
2024-06-29 11:41:50 +0200

    commit 2

--- src/main/java/BankService/jaz_s25378_nbp/controller/BankController.java
+++ src/main/java/BankService/jaz_s25378_nbp/controller/BankController.java
@@ -2,6 +2,9 @@ package BankService.jaz_s25378_nbp.controller;
 
 import BankService.jaz_s25378_nbp.model.CurRate;
 import BankService.jaz_s25378_nbp.service.BankService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +24,14 @@ public class BankController {
     }
 
 
+    @Operation(summary = "Oblicz średnią wartość walut w przedziale", description = "Komunikuje się z api nbp i zbiera dane o wartości waluty z x dni, po czym oblicza ich średnią wartość")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Film znaleziono i zwrócono"),
+            @ApiResponse(responseCode = "400", description = "Błędne dane w requescie"),
+            @ApiResponse(responseCode = "404", description = "Brak filmu w bazie"),
+            @ApiResponse(responseCode = "503", description = "Nie można otrzymać prawidłowej odpowiedzi od serwisu" +
+                    ", baza danych lub mikroserwis nie działa")
+    })
     @GetMapping("/{currency}/{startDate}/{endDate}")
     public ResponseEntity<CurRate> getAverageCurRate(@PathVariable String currency,
                                                      @PathVariable LocalDate startDate,
